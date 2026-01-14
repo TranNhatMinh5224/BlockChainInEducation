c23f67e9343ee753a33116aba295e65dd3058307
Minhxoan24 <142246049+Minhxoan24@users.noreply.github.com>
2026-01-15 06:18:08 +0700

    full

--- fabric-custom/GenKey.js
+++ fabric-custom/GenKey.js
@@ -2,36 +2,34 @@
 // Sinh business key pair cho Trường đại học
 // Dùng curve TNM5224 (business-layer)
 
-const fs = require('fs');
+const fs = require('fs'); 
 const ec = require('./tnm5224');
 
-// ===============================
 // 1. Sinh keypair
-// ===============================
+
 const keyPair = ec.genKeyPair();
 
-// ===============================
 // 2. Lấy private & public key
-// ===============================
+
 const privateKeyHex = keyPair.getPrivate('hex');
 const publicKeyHex = keyPair.getPublic('hex');
 
-// ===============================
+
 // 3. Lưu key ra file (THỰC TẾ)
-// ===============================
 
-// ⚠️ Private key: lưu bảo mật (KHÔNG commit Git)
-fs.writeFileSync(
-  './keys/truongA_private.key',
-  privateKeyHex,
-  { mode: 0o600 } // chỉ owner đọc/ghi
-);
 
-// Public key: có thể công bố
-fs.writeFileSync(
-  './keys/truongA_public.key',
-  publicKeyHex
-);
+//  Private key: lưu bảo mật (KHÔNG commit Git)
+// fs.writeFileSync(
+//   './keys/truongA_private.key',
+//   privateKeyHex,
+//   { mode: 0o600 } // chỉ owner đọc/ghi
+// );
+
+// // Public key: có thể công bố
+// fs.writeFileSync(
+//   './keys/truongA_public.key',
+//   publicKeyHex
+// );
 
 // ===============================
 // 4. Log để demo (KHÔNG dùng production)
--- fabric-custom/sign.js
+++ fabric-custom/sign.js
@@ -46,7 +46,6 @@ function signTranscript(filePath) {
   };
 }
 
-// ===============================
 // 3. Export
-// ===============================
+
 module.exports = signTranscript;
